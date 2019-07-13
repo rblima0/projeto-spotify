16967514e0de62f86f68d4bdb7ac3a360ff3ebb8
Rodrigo Banci <rblima0@gmail.com>
2019-07-13 14:20:14 -0300

    add search trigger

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
+
+searchEnterTrigger();
+
+const albumTracks = document.getElementById('album-tracks');
+const albumInfo = document.getElementById('album-info');
+
+const album = spotify.album.getAlbum('64lYVfx8NrXctVRKwGbPWB');
+
+album
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
 
 /* export const search = () => {};
 export const searchAlbums = () => {};
@@ -10,17 +21,3 @@ export const searchTracks = () => {};
 export const searchPlaylists = () => {}; */
 
 //export const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
-
-const albumList = document.getElementById('album-list');
-const albumTracks = document.getElementById('album-tracks');
-const albumInfo = document.getElementById('album-info');
-
-const albums = spotify.search.albums('Raça Negra');
-const album = spotify.album.getAlbum('64lYVfx8NrXctVRKwGbPWB');
-
-albums
-  .then(data => renderAlbums(data.albums.items, albumList));
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
